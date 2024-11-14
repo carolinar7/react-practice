cc30e84776e75a4559dd687cd2138b5d12549535
Frederick Santiago <frsantiago2001@gmail.com>
2024-11-14 02:25:45 -0500

    Update sidebar

--- react-exercises/src/components/sidebar/sidebar.tsx
+++ react-exercises/src/components/sidebar/sidebar.tsx
@@ -1,18 +1,19 @@
-import { SidebarButton } from "./sidebar_button";
-import reactSVG from "@/public/react.svg";
-
+import { SidebarButton } from "./sidebar_button"
+import reactSVG from "@/public/react.svg"
+import fsLogo from "@/public/fs_logo.svg"
+import Image from "next/image"
 
 export function Sidebar(): React.ReactElement {
   return (
-    <div className="flex flex-col h-screen w-fit rounded-3xl overflow-hidden gap-3 p-3">
-      <SidebarButton imageIcon={reactSVG} />
-      <SidebarButton imageIcon={reactSVG} />
-      <SidebarButton imageIcon={reactSVG} />
-      <SidebarButton imageIcon={reactSVG} />
-      <SidebarButton imageIcon={reactSVG} />
-      <SidebarButton imageIcon={reactSVG} />
-      <SidebarButton imageIcon={reactSVG} />
-      <SidebarButton imageIcon={reactSVG} />
+    <div className="m-4">
+      <div className="flex flex-col items-center justify-between h-full w-fit p-3 border border-foreground rounded-[41px]">
+        <div className="flex flex-col gap-3">
+          <SidebarButton imageIcon={reactSVG} />
+        </div>
+        <a href={"https://www.fredericksantiago.com"} target={"_blank"}>
+          <Image src={fsLogo} alt="logo" width={50} height={50} />
+        </a>
+      </div>
     </div>
   )
 }
