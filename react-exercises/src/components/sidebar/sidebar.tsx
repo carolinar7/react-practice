e87bec4293818bc3c34daf0a00395cfcfad3ef2e
Frederick Santiago <frsantiago2001@gmail.com>
2024-11-14 01:56:06 -0500

    Restyle the sidebar

--- react-exercises/src/components/sidebar/sidebar.tsx
+++ react-exercises/src/components/sidebar/sidebar.tsx
@@ -4,7 +4,7 @@ import reactSVG from "@/public/react.svg";
 
 export function Sidebar(): React.ReactElement {
   return (
-    <div className="flex flex-col h-screen w-fit border-r">
+    <div className="flex flex-col h-screen w-fit rounded-3xl overflow-hidden gap-3 p-3">
       <SidebarButton imageIcon={reactSVG} />
       <SidebarButton imageIcon={reactSVG} />
       <SidebarButton imageIcon={reactSVG} />
--- react-exercises/src/components/sidebar/sidebar_button.tsx
+++ react-exercises/src/components/sidebar/sidebar_button.tsx
@@ -8,7 +8,10 @@ interface SidebarButtonProps {
 
 export function SidebarButton({ imageIcon }: SidebarButtonProps): React.ReactElement {
   return (
-    <button className="max-w-fit h-18 rounded-none p-2 m-1" onClick={() => alert("yo")}>
+    <button 
+      className="max-w-fit h-50 rounded-full p-2 bg-primary hover:bg-secondary transition-colors"
+      onClick={() => alert("yo")
+    }>
       <Image src={imageIcon} alt="icon" width={50} height={50} />
     </button>
   )
